785433f0a2669d8c0d22462904c3f756cd73905a
rajuacure <raju.acure@gmail.com>
2026-07-25 16:53:18 +0600

    Fix cart update logic in script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ let total=0;
 cart.forEach((item,index)=>{
 
 total+=item.price;
-
+updateCartCount();
 html+=`
 <div style="background:#fff;padding:15px;margin:10px 0;border-radius:10px;box-shadow:0 2px 8px rgba(0,0,0,.1);">
 
